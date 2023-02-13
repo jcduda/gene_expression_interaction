2d315f87b4d11cfddcafdb6a9fc1ca258f31564b
jcduda <julia.duda@tu-dortmund.de>
2023-02-13 14:28:16 +0100

    Started code for main document

---
+++ 02_code/01_code_main.R
@@ -0,0 +1,17 @@
+
+# Code to generate all figures and analysis for the main part of the manuscript
+# "Benefit of using interaction effects for the analysis of high-dimensional time-response or dose-response data for two-group comparisons"
+#
+# Contact: duda@statistik.tu-dortmund.de
+
+# Setup ------------------------------------------------------------------------
+
+# ONLY the required packages
+
+
+# Data -------------------------------------------------------------------------
+
+# Note:
+# The data was provided by the IfADo and will be provided by request, given that the IfaDo allows it.
+
+load("./Data/010_gse.RData")
